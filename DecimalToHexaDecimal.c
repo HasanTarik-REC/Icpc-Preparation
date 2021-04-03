95b9c01328658103a0319d0a62f600753819b7c8
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-04-03 10:43:02 +0600

    Decimal To Hexa Decimal

---
+++ DecimalToHexaDecimal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any decimal number:\n");
+    scanf("%d",&num);
+    printf("The Hexa Decimal number is: %X ",num);
+    return 0;
+
+}
